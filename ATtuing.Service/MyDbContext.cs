f7627d1246a9c00149a0b5bf5c02616f4a35ac62
hhy <1300776935@qq.com>
2017-09-12 11:45:41 +0800

    dbcontext修改

--- ATtuing.Service/MyDbContext.cs
+++ ATtuing.Service/MyDbContext.cs
@@ -21,6 +21,6 @@ namespace ATtuing.Service
             modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
         }
         public DbSet<EQEventEntity> EQEvents { get; set; }
-        public DbSet<EQEventTaskEntity> EQEventTasks { get; set; }
+        public DbSet<EQEventTaskEntity>  EQEventTasks { get; set; }
     }
 }
