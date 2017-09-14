565707eb48dc53423d4c2295ed8f71972b19ef15
hhy <1300776935@qq.com>
2017-09-14 17:51:06 +0800

    单元测试代码提交

---
+++ ATtuing.UnitTest/EQEventUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ATtuing.Service;
+
+namespace ATtuing.UnitTest
+{
+    [TestClass]
+    public class EQEventUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Mapper映射
+            MapperConfiguration.Configure();
+            EQEventService service = new EQEventService();
+           var eqevent= service.GetById();
+            Assert.AreEqual(eqevent.EQId, "20150109121037");
+            //            public ActionResult Index()
+            //{
+            //    var xx = EQEventService.GetById();
+            //    return Json(xx);
+            //}
+            //public ActionResult IndexSql()
+            //{
+            //    var xx = EQEventService.GetByIdSql();
+            //    return Json(xx);
+            //}
+            //public ActionResult IndexInclude()
+            //{
+            //    var xx = EQEventService.IncludeEQEvent();
+            //    return Json(xx);
+            //}
+            ////一对多最佳测试
+            //public ActionResult Index1()
+            //{
+            //    var xx = EQEventService.GetEQEventTask();
+            //    return View();
+            //}
+        }
+    }
+}
